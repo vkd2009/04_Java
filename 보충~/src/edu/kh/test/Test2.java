8735cb08f5f1d8ee6b1f89df9d14e9aec4589aae
inje <vkd2009@naver.com>
2024-02-22 17:39:09 +0900

    20240221_보충 시험 추가 내용

---
+++ 보충~/src/edu/kh/test/Test2.java
@@ -0,0 +1,44 @@
+package edu.kh.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Test2 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		List<String> nameList = new ArrayList<String>();
+		
+		while(true) {
+			System.out.print("추가할 이름을 입력하세요 : ");
+			String name = sc.next();
+						
+			if(name.equals("clear")) { 
+				System.out.println("이름 목록을 초기화 합니다.");
+				nameList.clear();
+				continue; 
+			}
+			
+			if(name.equals("exit")){			
+				System.out.println("<<입력 종료>>");
+				
+				break; // break;
+			}		
+			
+			if(nameList.contains(name)) {
+				
+				System.out.println("<이미 존재하는 이름입니다>"); 
+			}else {	
+				nameList.add(name);
+				System.out.println(name + " 추가 완료.");
+			}		
+		}
+		
+		for(String name : nameList) {
+			 System.out.println(name);
+		}
+		
+		
+	}
+}
